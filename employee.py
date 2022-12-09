bf89193d6dff78cf3de0f1f72d5291d1cb97cd14
Jiahao Cui <k20108456@kcl.ac.uk>
2022-12-09 01:23:40 +0000

    finish

--- employee.py
+++ employee.py
@@ -44,7 +44,7 @@ class ContractCommission:
             return self.contracts * self.commission
 
         def __str__(self):
-            string = f'a commission for {self.contracts} contract(s) at {self.commission}'
+            string = f'a commission for {self.contracts} contract(s) at {self.commission}/contract'
             return string
 
 class Employee:
